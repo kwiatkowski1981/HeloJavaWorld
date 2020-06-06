6942384ff8e3d8a3771f7a5fb4690df17e7b0a49
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-06 12:17:29 +0200

    added: first operations on variables in java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,1 @@
-class Simple {
-   public static void main(String[] args) {
-      System.out.print("Nasz drugi program");
-
-   }
-}
-
-class yoyoyo {
-   public static void main(String[] args) {
-      System.out.println("yoyyoyoyo\t" +
-              "jojojojo\t" +
-              "elooooooomordo");
-   }
-}
-
-
 
---
+++ src/zmienne.java
@@ -0,0 +1,17 @@
+public class zmienne {
+    public static void main(String[] args) {
+        String firstName = "Kuba";
+        String lastName = "Kwiatkowski";
+        int age = 39;
+        int weight = 86;
+        double height = 187.5;
+
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+        System.out.println(height + "cm");
+        System.out.println(weight + "kg");
+
+    }
+}
